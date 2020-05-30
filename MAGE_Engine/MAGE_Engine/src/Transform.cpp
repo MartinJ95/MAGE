1ca9d5efdd8055f996f8f23f69e10745438ab2e8
MartinJ95 <46921574+MartinJ95@users.noreply.github.com>
2020-05-30 14:22:22 +0100

    transform component added
    entity's can now have a transform component containing position, scale and rotation values.

---
+++ MAGE_Engine/MAGE_Engine/src/Transform.cpp
@@ -0,0 +1,24 @@
+#include "Transform.h"
+#include "Entity.h"
+#include "World.h"
+
+Transform::Transform(Entity& entity) :
+	Component(entity),
+	m_position(0, 0, 0),
+	m_scale(1, 1, 1),
+	m_rotation(0, 0, 0)
+{
+
+}
+
+void Transform::Update(World & world)
+{
+}
+
+void Transform::FixedUpdate(World & world)
+{
+}
+
+Transform::~Transform()
+{
+}
---
+++ MAGE_Engine/MAGE_Engine/src/Transform.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Component.h"
+#include "Maths.h"
+
+class Entity;
+class World;
+
+class Transform :
+	public Component
+{
+public:
+	Transform(Entity &entity);
+	void Update(World &world) override;
+	void FixedUpdate(World &world) override;
+	~Transform();
+	Vector3f m_position;
+	Vector3f m_scale;
+	Vector3f m_rotation; //(vec3(yaw, pitch, roll))
+};
+
